22e758e530bf58b2a20d70f2fff2ca7a654f24cd
Gregor Traven <gregor.traven@astro.lu.se>
2019-04-09 19:48:26 +0200

    add s2 to Payne

--- src/pythonModules/fourgp_payne/fourgp_payne/ting_test_nn.py
+++ src/pythonModules/fourgp_payne/fourgp_payne/ting_test_nn.py
@@ -18,14 +18,18 @@ def sigmoid_def(z):
 # ---------------------------------------------------------------------------
 # define function to perform testing step in batch
 def fit_spectrum(params):
-    spec_no, num_labels, Y_u_all, Y_u_all_err, censor_mask, x_min, x_max, w_array_0, w_array_1, w_array_2, b_array_0, b_array_1, b_array_2 = params
+    spec_no, num_labels, Y_u_all, Y_u_all_err, censor_mask, x_min, x_max, w_array_0, w_array_1, w_array_2, b_array_0, b_array_1, b_array_2, s2 = params
 
     # Fudge an offset due to Payne spiking at the left edge of wav. range - NO LONGER NEEDED so set to 0
     fudge_offset = 0
 
     # Deal with pixels which are nan    
     spectrum = Y_u_all[:, spec_no][censor_mask][fudge_offset:]
-    spectrum_errors = np.sqrt(1./Y_u_all_err[:, spec_no][censor_mask][fudge_offset:]) # we need sigma here, not 1/sigma^2
+    #spectrum_errors = np.sqrt(1./Y_u_all_err[:, spec_no][censor_mask][fudge_offset:]) # we need sigma here, not 1/sigma^2
+
+    adjusted_ivar = Y_u_all_err[:, spec_no]/(1. + Y_u_all_err[:, spec_no] * s2)
+    spectrum_errors = np.sqrt(1.0/adjusted_ivar)[censor_mask][fudge_offset:]
+
     bad_pixels = np.isnan(spectrum * spectrum_errors)
     spectrum[bad_pixels] = 1.
     spectrum_errors[bad_pixels] = 9999.
@@ -66,7 +70,6 @@ def fit_spectrum(params):
 
     return np.concatenate([popt, uncertainties])
 
-
 def test_nn(payne_status, threads, num_labels, test_spectra, test_spectra_errors, censors):
     # set number of threads per CPU
     os.environ['OMP_NUM_THREADS'] = '{:d}'.format(1)
@@ -89,6 +92,7 @@ def test_nn(payne_status, threads, num_labels, test_spectra, test_spectra_errors
     b_array_0 = payne_status["b_array_0"]
     b_array_1 = payne_status["b_array_1"]
     b_array_2 = payne_status["b_array_2"]
+    s2 = payne_status["s2"]
     x_min = payne_status["x_min"][:num_labels]
     x_max = payne_status["x_max"][:num_labels]
 
@@ -99,12 +103,14 @@ def test_nn(payne_status, threads, num_labels, test_spectra, test_spectra_errors
 
     # ============================================================================
     # fit spectra
-    params = [num_labels, Y_u_all, Y_u_all_err, censors['[Fe/H]'], x_min, x_max, w_array_0, w_array_1, w_array_2, b_array_0, b_array_1, b_array_2]
+    params = [num_labels, Y_u_all, Y_u_all_err, censors['[Fe/H]'], x_min, x_max, w_array_0, w_array_1, w_array_2, b_array_0, b_array_1, b_array_2, s2]
+
+    #print(np.sqrt(1./Y_u_all_err[:, 0]))
+    #print(np.sqrt(s2))
 
     # Fitting in parallel
-    with Pool(num_CPU) as pool:
-        recovered_results = np.array(pool.map(fit_spectrum,
-                                              [[i]+params for i in range(Y_u_all.shape[1])])).T
+    
+    recovered_results = np.array(fit_spectrum([0]+params)).T
 
     # Fitting in serial
     # recovered_results = []
@@ -119,35 +125,37 @@ def test_nn(payne_status, threads, num_labels, test_spectra, test_spectra_errors
     uncertainties = []
 
     # loop over all spectra
-    for j in range(recovered_results.shape[1]):
-        labels = recovered_results[:num_labels, j]
-        ind_invalid = (labels < -100.)
-        labels = (labels + 0.5) * (x_max - x_min) + x_min
-        labels[ind_invalid] = -999.
-        results.append(labels)
+    j = 0
+    #for j in range(recovered_results.shape[1]):
 
-        uncert = recovered_results[num_labels:, j]
-        uncert = (uncert) * (x_max - x_min)
-        uncertainties.append(uncert)
+    labels = recovered_results[:num_labels]
+    ind_invalid = (labels < -100.)
+    labels = (labels + 0.5) * (x_max - x_min) + x_min
+    labels[ind_invalid] = -999.
+    results.append(labels)
 
-        predict_flux = w_array_2 * sigmoid_def(np.sum(w_array_1 * (sigmoid_def(np.dot(
-            w_array_0, recovered_results[:num_labels, j]) + b_array_0)), axis=1) + b_array_1) \
-                       + b_array_2
+    uncert = recovered_results[num_labels:]
+    uncert = (uncert) * (x_max - x_min)
+    uncertainties.append(uncert)
 
-        # radial velocity
-        # f_interp = interpolate.interp1d(wavelength_template, predict_flux,
-        #                                 bounds_error=False, kind="linear", fill_value="extrapolate")
-        # predict_flux = f_interp(wavelength_template \
-        #                         + recovered_results[-1, j] * wavelength_template / 10 ** 5)
-        chi2.append(np.mean((predict_flux - Y_u_all[:, j]) ** 2 * (Y_u_all_err[:, j])))
+    predict_flux = w_array_2 * sigmoid_def(np.sum(w_array_1 * (sigmoid_def(np.dot(
+        w_array_0, recovered_results[:num_labels]) + b_array_0)), axis=1) + b_array_1) \
+                   + b_array_2
+
+    # radial velocity
+    # f_interp = interpolate.interp1d(wavelength_template, predict_flux,
+    #                                 bounds_error=False, kind="linear", fill_value="extrapolate")
+    # predict_flux = f_interp(wavelength_template \
+    #                         + recovered_results[-1, j] * wavelength_template / 10 ** 5)
+    chi2.append(np.mean((predict_flux - Y_u_all[:, j]) ** 2 * (Y_u_all_err[:, j])))
 
     if False:
         import matplotlib
         # matplotlib.use('Agg')
         import matplotlib.pyplot as plt
         fig = plt.figure(figsize=(12, 8), dpi=200)
-        plt.plot(Y_u_all[:, j], color='black')
-        plt.plot(predict_flux, color='red')
+        plt.plot(Y_u_all[:, j][censors['[Fe/H]']], color='black')
+        plt.plot(predict_flux[censors['[Fe/H]']], color='red')
         plt.show()
     chi2 = np.array(chi2)
 
